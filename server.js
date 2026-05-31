958641e3b7571ac969479e2f0773acc7015e7fed
Manjunath <manjunathgs908@gmail.com>
2026-05-31 13:56:19 +0530

    Fix CORS for production

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: ['https://medifleet-frontend-1.onrender.com', 'http://localhost:3000'], credentials: true }));
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(morgan('dev'));
 

406a2d1fdb1f7e8c326f9e6a88369bbfc93b6a63
Manjunath <manjunathgs908@gmail.com>
2026-05-31 15:03:49 +0530

    Add CORS for app

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 'use strict';
 const express    = require('express');
 const mongoose   = require('mongoose');
@@ -13,7 +12,15 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ['https://medifleet-frontend-1.onrender.com', 'http://localhost:3000'], credentials: true }));
+app.use(cors({
+  origin: [
+    'https://medifleet-frontend-1.onrender.com',
+    'http://localhost:3000',
+    'http://localhost:8081',
+    'http://localhost:19006'
+  ],
+  credentials: true
+}));
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(morgan('dev'));
 
